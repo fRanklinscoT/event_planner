962dfdd859161d4e6b2f0c33f5fbcab7ea3ba592
Frankllin $c0tt <dellabrentraolane@gmail.com>
2024-09-27 18:57:22 +0200

    Create Footer.jsx

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';  
+
+const MyFooter = () => {
+    return (
+        <>
+        <footer>
+            <p>&copy;Illumidev solutions</p>
+        </footer>
+        </>
+    )
+}
+export default MyFooter
